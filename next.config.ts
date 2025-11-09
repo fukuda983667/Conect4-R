8c24a9cfd3345d2f7ddf79f485aa9519a8c036ac
fukuda983667 <sfu983667@gmail.com>
2025-11-10 05:57:23 +0900

    vercelデプロイ時にESLintエラーを許容

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // ← ESLint エラーでもビルドを止めない
+  },
 };
 
 export default nextConfig;
